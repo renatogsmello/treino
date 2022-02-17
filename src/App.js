09895efd80e51dc071a6dfc06f04e33bbd489a01
renatogsmello <renato.mello@globalhitss.com.br>
2022-02-17 16:25:24 -0300

    commit inicial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import SideBar from "./components/sidebar"
+import "./App.css"
+
+function App() {
+	return (
+		<div className="flex">
+			<SideBar />
+		</div>
+	)
+}
+
+export default App
---
+++ src/components/sidebar/index.js
@@ -0,0 +1,26 @@
+import React from "react"
+import { BsPlus, BsFillLightningFill, BsGearFill } from "react-icons/bs"
+import { FaArrowRight, FaFire, FaPoo } from "react-icons/fa"
+
+export default function SideBar() {
+	return (
+		<div className=" fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg">
+			<SideBarIcon icon={<FaArrowRight size="20" />} />
+			<SideBarIcon icon={<FaFire size="28" />} />
+			<SideBarIcon icon={<BsPlus size="32" />} />
+			<SideBarIcon icon={<BsFillLightningFill size="20" />} />
+			<SideBarIcon icon={<FaPoo size="20" />} />
+			<Divider />
+			<SideBarIcon icon={<BsGearFill size="22" />} />
+		</div>
+	)
+}
+
+const SideBarIcon = ({ icon, text = "tooltip 💡" }) => (
+	<div className="sidebar-icon group">
+		{icon}
+		<span className="sidebar-tooltip group-hover:scale-100">{text}</span>
+	</div>
+)
+
+const Divider = () => <hr className="sidebar-hr" />
